049131dc2fedf946e60daaee7cb3126d1dc86398
Anil Nagar <anil@keriton.com>
2020-05-30 18:46:04 +0530

    first commit

--- test.py
+++ test.py
@@ -35,6 +35,9 @@ from tornado.ioloop import PeriodicCallback
 
 
 class WSHandler(tornado.websocket.WebSocketHandler):
+	bd_addr = '00:19:08:35:F1:7F'
+	port = 1
+
     def check_origin(self, origin):
         return True
 
@@ -49,19 +52,17 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         pass
 
     def send_temp(self):
-		bd_addr = '00:19:08:35:F1:7F'
-		port = 1
-		sock = BluetoothSocket(RFCOMM)
-		sock.connect((bd_addr, port))
+			sock = BluetoothSocket(RFCOMM)
+			sock.connect((bd_addr, port))
 
-		print('waiting')
+			print('waiting')
 
-		while True:
-			data = sock.recv(10)
-			print(data.decode())
-			self.write_message("Current")
+			while True:
+				data = sock.recv(10)
+				print(data.decode())
+				self.write_message("Current")
 
-        sock.close()
+			sock.close()
 
     def on_close(self):
         self.callback.stop()
